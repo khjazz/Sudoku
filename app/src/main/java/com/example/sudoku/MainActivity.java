0a9babbf76128f01391a9a21b4eb359245de078d
khjazz <homeworkccc@gmail.com>
2023-11-18 23:50:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.sudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button gameButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gameButton = findViewById(R.id.game_button);
+
+        gameButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                        MainActivity.this.startActivity(intent);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        int itemId = item.getItemId();
+        if (itemId == R.id.setting) {
+            Intent intent = new Intent(MainActivity.this, PreferenceActivity.class);
+            MainActivity.this.startActivity(intent);
+            return true;
+        } else if (itemId == R.id.about) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Sudoku Game")
+                    .setMessage("Sudoku is a logic-based, combinatorial number-placement puzzle..")
+                    .setNeutralButton(android.R.string.ok, null)
+                    .show();
+            return true;
+        }
+        return false;
+    }
+
+}
